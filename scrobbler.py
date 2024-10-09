6a5d75028ebb9610f24913b7024acf201e98a9a1
Aks1389 <aksnv511@gmail.com>
2024-10-09 22:25:13 +0300

    getting session works

--- scrobbler.py
+++ scrobbler.py
@@ -2,8 +2,11 @@ import sys
 import os
 import requests
 from dotenv import load_dotenv
-import hashlib
+from hashlib import md5
 import logging
+import webbrowser
+import json
+from pathlib import Path
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
@@ -14,40 +17,62 @@ api_key = os.getenv("API_KEY")
 api_secret = os.getenv("API_SECRET")
 
 global token
-global session_key
+global session
 
 def get_token():
     res = requests.get(f'{host}/2.0/?method=auth.gettoken&api_key={api_key}&format=json')
     logging.info(f"[Auth] Get token status: {res.status_code}")
+    if not res.status_code == 200:
+        raise ValueError('Getting token failure.')
     return res.json()['token']
+
+def auth():
+    webbrowser.open(f"http://www.last.fm/api/auth/?api_key={api_key}&token={token}", new=0, autoraise=True)
 
 def get_session(token):
     signature = get_api_method_signature('auth.getSession')
-    res = requests.get(f'{host}/2.0/?method=auth.getSession&token={token}&api_key={api_key}&api_sig={signature}')
+    res = requests.get(f'{host}/2.0/?method=auth.getSession&api_key={api_key}&token={token}&api_sig={signature}&format=json')
     logging.info(f"[Auth] Get session status: {res.status_code}")
+    if not res.status_code == 200:
+        raise ValueError('Getting session failure.')
     return res.json()
+
+def get_existing_session():
+    return
+
+def save_session(session , token):
+    return
+
+def authenticate():
+    global token
+    global session
+    if(Path('./session/session1.json').exists()):
+       token, session = get_existing_session()
+    else:
+        token = get_token()
+        auth()
+        session = get_session(token)
+        save_session(session, token)
 
 def get_api_method_signature(method_name):
     global token
     method_sign_string = f'api_key{api_key}method{method_name}token{token}{api_secret}'
-    return hashlib.md5(method_sign_string.encode()).hexdigest()
+    return md5(method_sign_string.encode('utf-8')).hexdigest()
 
 def scrobble():
     global token
-    token = get_token()
-    get_session(token)
+    
 
 def nowPlaying():
-    global token
-    global session_key
     
-    token = get_token()
-    session_key = get_session(token)
+    
+    
     signature = get_api_method_signature('track.updateNowPlaying')
-    logging.info(f'signature: {signature}')
+    res = requests.post(f"{host}/2.0/?method=track.updateNowPlaying")
 
 
 if __name__ == '__main__':
+    authenticate()
     # scrobble()
     nowPlaying()
     # sys.exit(0)
