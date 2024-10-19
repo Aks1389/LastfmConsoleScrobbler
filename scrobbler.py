6057151431a4eac9b445186cb4acf8a5f9bc61e1
Aks1389 <aksnv511@gmail.com>
2024-10-19 20:56:45 +0300

    nowPlaying method works

--- scrobbler.py
+++ scrobbler.py
@@ -28,9 +28,11 @@ def get_token():
 
 def auth():
     webbrowser.open(f"http://www.last.fm/api/auth/?api_key={api_key}&token={token}", new=0, autoraise=True)
+    input('Press Enter to continue...')
 
 def get_session(token):
-    signature = get_api_method_signature('auth.getSession')
+    sign_params = {'method': 'auth.getSession', 'api_key': api_key, 'token': token}
+    signature = get_api_method_signature(sign_params)
     res = requests.get(f'{host}/2.0/?method=auth.getSession&api_key={api_key}&token={token}&api_sig={signature}&format=json')
     logging.info(f"[Auth] Get session status: {res.status_code}")
     if not res.status_code == 200:
@@ -46,7 +48,7 @@ def save_session(path, session , token):
     session['session']['token'] = token
     json_object = json.dumps(session, indent=4)
  
-    with open(path, "w") as outfile:
+    with open(path, "x") as outfile:
         outfile.write(json_object)
 
 def authenticate():
@@ -55,7 +57,7 @@ def authenticate():
     path = './session/session1.json'
     if(Path(path).exists()):
         logging.info(f"[Auth] An existing session found.")
-        token, session = get_existing_session(path)
+        session, token = get_existing_session(path)
     else:
         logging.info(f"[Auth] There is no session found. Will be created a new one.")
         token = get_token()
@@ -63,25 +65,54 @@ def authenticate():
         session = get_session(token)
         save_session(path, session, token)
 
-def get_api_method_signature(method_name):
+def get_api_method_signature(params_dict):
     global token
-    method_sign_string = f'api_key{api_key}method{method_name}token{token}{api_secret}'
+    keys = sorted(params_dict.keys())
+    method_sign_string = [k + str(params_dict[k]) for k in keys]
+    method_sign_string = "".join(method_sign_string) + api_secret
     return md5(method_sign_string.encode('utf-8')).hexdigest()
 
 def scrobble():
-    return
-    
-
-def nowPlaying():
-    
-    
-    
     signature = get_api_method_signature('track.updateNowPlaying')
-    # res = requests.post(f"{host}/2.0/?method=track.updateNowPlaying")
+    body = {
+        
+        "api_key": api_key,
+        "api_sig": signature,
+        "sk": session
+        }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    res = requests.post(url=f"{host}/2.0/", data=body, headers=headers)
+    if not res.status_code == 200:
+        print(res.text)
+        raise ValueError('Scrobbling didn\'t worked.')
+    logging.info("Scrobbling:" + res.json())
+    
+def nowPlaying():
+    """api: https://www.last.fm/api/show/track.updateNowPlaying"""
+    body = {
+        "method": 'track.updateNowPlaying',
+        "artist": 'Dark Tranquillity',
+        "track": 'Damage Done',
+        "album": 'Damage Done',
+        "api_key": api_key,
+        "sk": session
+        }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    signature = get_api_method_signature(body)
+    body['api_sig'] = signature
+    res = requests.post(url=f"{host}/2.0/", data=body, headers=headers)
+    if not res.status_code == 200:
+        print(res.text)
+        raise ValueError('Now playing didn\'t worked.')
+    logging.info("NowPlaying:" + res.text)
 
 
 if __name__ == '__main__':
     authenticate()
     # scrobble()
-    nowPlaying()
+    # nowPlaying()
     # sys.exit(0)
